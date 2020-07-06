1d4fb7d03c6b0f61eb12e05c3ffa5c46acf9cda1
Vishal K <vishal.km@iitg.ac.in>
2020-07-06 10:24:54 +0530

    Create Logo.js

---
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+import logo from 'https://raw.githubusercontent.com/Medialabiitg/medialab-underconstruction/master/static/Media%20lab%20logo%20title.png';
+export default styled.div`
+  width: 160px;
+  height: 36px;
+  background-size: contain;
+  background: url(${logo}) center no-repeat;
+`;
